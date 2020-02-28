0b6879794aed224d81f1176f5e113730b1015abc
Erik Ghukasyan <erik.ghukasyan@labo-ve.fr>
2020-02-28 21:37:07 +0100

    Add scores and result of game

--- core/src/com/games/memory/Game.java
+++ core/src/com/games/memory/Game.java
@@ -1,21 +1,28 @@
 package com.games.memory;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 class Game  {
-
-    private final int DECKS_COUNT = 4;
-    private final int CARDS_COUNT = 4;
+    private final String WIN_IMAGE_URL = "images/PNG/finish.png";
+    private final int DECKS_COUNT = 2;
+    private final int CARDS_COUNT = 8;
     private boolean equality;
     private Card card1, card2;
     private MyInputProcessor inputProcessor;
     private List<Deck> deckList;
     private int countOfPairs;
+    private int countOfFalses;
+    private int score;
+    private Sprite gameFinishSprite;
+    private SpriteController spriteController;
 
     private List<Card> globalList;
     private Deck deck, anotherDeck;
+    private boolean gameIsFinished;
 
     Game(){
         inputProcessor = new MyInputProcessor();
@@ -38,18 +45,25 @@ class Game  {
                 e.printStackTrace();
             }
         }
-
-
         Collections.shuffle(globalList);
     }
 
     void startGame(Card card) {
+        if(countOfPairs == CARDS_COUNT * DECKS_COUNT/2) {
+            spriteController = new SpriteController();
+            gameFinishSprite = spriteController.initSprite(WIN_IMAGE_URL);
+            gameIsFinished = true;
+            score = ((DECKS_COUNT * CARDS_COUNT)*(DECKS_COUNT * CARDS_COUNT)/countOfFalses * 2);
+            System.out.println("You Win. You have a : " + score + " points");
+            return;
+        }
         if (card1 == null) {
             card1 = openIfClicked(card);
         } else if(card2 == null) {
             if(card == card1) return;
             card2 = openIfClicked(card);
             compare(card1, card2);
+
         } else {
             openIfClicked(card);
         }
@@ -61,10 +75,10 @@ class Game  {
 
     private Card openIfClicked(Card card) {
         if (inputProcessor.leftClick()) {
+
             int mouseY = MemoryGame.HEIGHT - inputProcessor.getY();
             if (!card.isVisibility() && (inputProcessor.getX() > card.getX() && inputProcessor.getX() < card.getX() + card.WIDTH) &&
                     (mouseY > card.getY() && mouseY < card.getY() + card.HEIGHT)) {
-
                 if(card1 != null && card2 != null) {
                     if (!equality) {
                         closeCard(card1);
@@ -73,7 +87,16 @@ class Game  {
 
                     card1 = card;
                     card2 = null;
+                } else if(card1 != null && card2 == null) {
+                    if(card1.equals(card)) {
+                        countOfPairs++;
+                        System.out.println("countOfPairs - " + countOfPairs);
+                    } else {
+                        countOfFalses++;
+                        System.out.println("countOfFalses - " + countOfFalses);
+                    }
                 }
+
                 openCard(card);
                 return card;
             }
@@ -95,7 +118,23 @@ class Game  {
         return deckList;
     }
 
+    public boolean isGameIsFinished() {
+        return gameIsFinished;
+    }
+
     public List<Card> getGlobalList() {
         return globalList;
+    }
+
+    public Sprite getGameFinishSprite() {
+        return gameFinishSprite;
+    }
+
+    public void setGameFinishSprite(Sprite gameFinishSprite) {
+        this.gameFinishSprite = gameFinishSprite;
+    }
+
+    public int getScore() {
+        return score;
     }
 }
--- core/src/com/games/memory/GameMainScreen.java
+++ core/src/com/games/memory/GameMainScreen.java
@@ -2,13 +2,16 @@ package com.games.memory;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameMainScreen implements Screen {
     Game newGame;
     private SpriteBatch batch;
     private MemoryGame game;
+    private BitmapFont font;
 
 
     public GameMainScreen(MemoryGame game) {
@@ -17,6 +20,8 @@ public class GameMainScreen implements Screen {
 
     @Override
     public void show() {
+        font = new BitmapFont();
+        font.setColor(Color.RED);
         batch = new SpriteBatch();
         newGame = new Game();
     }
@@ -26,23 +31,22 @@ public class GameMainScreen implements Screen {
         Gdx.gl.glClearColor(0.4f, 0.7f, 0.7f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-
             int x1 = 20, y1 = 50;
-
-                for (Card c: newGame.getGlobalList()) {
-                    c.setX(x1);
-                    c.setY(y1);
-
-                    newGame.startGame(c);
-
-                    batch.draw(c.getCardSprite(), x1, y1);
-
-                    x1 += 90;
-                    if(x1 >= MemoryGame.WIDTH-80) {
-                        x1 = 20;
-                        y1 += 130;
-                    }
+            for (Card c: newGame.getGlobalList()) {
+                c.setX(x1);
+                c.setY(y1);
+                newGame.startGame(c);
+                if(newGame.isGameIsFinished()) {
+                    batch.draw(newGame.getGameFinishSprite(), 250, 350);
+                    font.draw(batch, "Your score is " + newGame.getScore(), 20, 600);
                 }
+                batch.draw(c.getCardSprite(), x1, y1);
+                x1 += 90;
+                if(x1 >= MemoryGame.WIDTH-80) {
+                    x1 = 20;
+                    y1 += 130;
+                }
+            }
 
 
         batch.end();
